2337c3d5788a33c2a6aa8e1aaeba374684b3daec
Tao Ho <vantao.dev@gmail.com>
2025-08-25 14:04:18 +0700

    remove console

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -18,8 +18,6 @@ export default async function Page(props: {
   const currentPage = Number(searchParams?.page) || 1;
   const totalPages = await fetchInvoicesPages(query);
 
-  console.log("query", query);
-
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
