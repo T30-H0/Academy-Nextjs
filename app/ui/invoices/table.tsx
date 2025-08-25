e7a899fed1e070f865b21ce81e822e4b90dd0b75
Tao Ho <vantao.dev@gmail.com>
2025-08-25 15:43:53 +0700

    done chapter 12 - CRUD invoice

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -11,7 +11,6 @@ export default async function InvoicesTable({
   query: string;
   currentPage: number;
 }) {
-  console.log("query", query, currentPage);
   const invoices = await fetchFilteredInvoices(query, currentPage);
 
   return (
